23b3a4c4f7e0b14b2c76eda177b6b72e1905e32c
Mayur0018 <mayurnish18@gmail.com>
2025-01-23 00:22:07 +0530

    commit

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const userRoutes = require("./routes/user");
+const mongoose = require("mongoose");
 
-app.set("view engine","ejs");
-app.set("views",path.resolve("./views"));
-
-app.get("/",(req,res)=>{
-    res.render("home")
-})
-app.listen(PORT, () => console.log(`Server Was Started :${PORT}`));
+mongoose
+  .connect("mongodb+srv://mayurnish18:mayur123@cluster0.ks1hcpc.mongodb.net/")
+  .then((e) => console.log("MongoDb Connected"));
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use("/user", userRoutes);
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.listen(PORT, () => console.log(`Server Was S    tarted :${PORT}`));
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,21 @@
 const { Router } = require("express");
 const router = Router();
-
+const User = require("../models/user");
 router.get("/signin", (req, res) => {
-  return req.render("signin");
+  return res.render("signin");
 });
 
 router.get("/signUp", (req, res) => {
-  return req.render("signup");
+  return res.render("signUp");
 });
 
-router.post("/signUp", (req, res) => {
+router.post("/signUp", async (req, res) => {
   const { FullName, email, password } = req.body;
+  await User.create({
+    FullName,
+    email,
+    password
+  });
+  return res.redirect("/");
 });
 module.exports = router;
