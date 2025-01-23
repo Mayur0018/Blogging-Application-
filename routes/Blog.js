06a472ccad8c18af1a321097271b9ee11ae49cf0
Mayur0018 <mayurnish18@gmail.com>
2025-01-23 17:44:46 +0530

    commit

--- routes/Blog.js
+++ routes/Blog.js
@@ -1,9 +1,25 @@
 const { Router } = require("express");
 const router = Router();
-
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve('/public/uploads'))
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 router.get("/add-new", (req, res) => {
-  return res.render("addBlog",{
-    user:req.user
+  return res.render("addBlog", {
+    user: req.user,
   });
+});
+router.post("/", (req, res) => {
+  console.log(req.body);
+  return res.redirect("/")
 });
 module.exports = router;
