6d90052bacc1342dbf9df36e9f5f503eb0cf094f
Mayur0018 <mayurnish18@gmail.com>
2025-01-23 12:26:40 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user"); // Ensure this file exists
 const app = express();
 const PORT = 8000;
-
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 // MongoDB Connection
 mongoose
   .connect("mongodb+srv://mayurnish18:mayur123@cluster0.ks1hcpc.mongodb.net/")
@@ -14,7 +17,8 @@ mongoose
 // Middleware
 app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded request bodies
-
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 // EJS Setup
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
@@ -22,7 +26,9 @@ app.set("views", path.resolve("./views"));
 // Routes
 app.use("/user", userRoutes);
 app.get("/", (req, res) => {
-  res.render("home"); // Ensure there's a 'home.ejs' file in the 'views' directory
+  res.render("home", {
+    user: req.user,
+  });
 });
 
 // Start Server
